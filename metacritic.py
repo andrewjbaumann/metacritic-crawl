04e3a467685ff181243b6884688463a04c7c0ec9
Andrew Baumann <baumann.andrew14@gmail.com>
2015-11-06 17:58:31 -0500

    cleaning up metacritic.py

--- metacritic.py
+++ metacritic.py
@@ -1,8 +1,7 @@
 '''
-	@author: andrew baumann 
-	@purpose: crawl metacritic's scores, and assemble some data (avg, etc)
-	@contact: abaumann@columnit.com	
-	@additional: using Kimino 
+@author: andrew baumann 
+@purpose: crawl metacritic's scores, and assemble some data (avg, etc)
+@contact: abaumann@columnit.com	
 '''
 import json
 from multiprocessing import Process, freeze_support
@@ -14,17 +13,3 @@ if __name__ == '__main__':
 	freeze_support()
 	crawl = Crawler()
 	crawl.set_crawl()
-
-
-
-# totals.append(crawl.years_critic_crawler())
-
-# totals.append(crawl.critic_crawler())
-# totals.append(crawl.overall_critic_crawler())
-# totals.append(crawl.user_crawler())
-# totals.append(crawl.overall_user_crawler())
-
-# added_totals = reduce(add, totals)
-# complete_avg = added_totals / len(totals)
-
-# print complete_avg
